c7c16eeceb1a8679d792a14b0f06e68163a98b8f
Kotyuzhanskiy <32913672+Kotyuzhanskiy@users.noreply.github.com>
2018-04-05 01:41:21 +0300

    Add files via upload

--- app/models/models.py
+++ app/models/models.py
@@ -1,13 +1,13 @@
 from app import db
-from flask_login import UserMixin
 
-#class User(db.Model):
-class User(UserMixin, db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
+
+    Name = db.Column(db.String(64), index=True, unique=False)
+    PatronymicName = db.Column(db.String(64), index=True, unique=False)
+    SurName = db.Column(db.String(64), index=True, unique=False)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,7 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, url_for, request
+from flask import render_template, flash, redirect, url_for
 from app import app
-from app.controllers.forms import LoginForm, SearchForm, SignUpForm
-from flask_login import current_user, login_user, login_required
-from werkzeug.urls import url_parse
-from app.models.models import User
-from app import db
+from app.controllers.forms import LoginForm, SearchForm
 
 @app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
@@ -16,7 +12,6 @@ def index():
         return redirect(url_for('search'))
     return render_template('index.html', title='Welcome', form=form)
 
-
 @app.route('/search', methods=['GET', 'POST'])
 def search():
     # TO DO
@@ -25,44 +20,21 @@ def search():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
+    # TO DO
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-        return redirect(url_for('index'))
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('account'))
     return render_template('login.html', title='Войти', form=form)
 
-@app.route
-@login_required
+@app.route('/account', methods=['GET', 'POST'])
 def account():
     #TO DO
     return render_template('account.html', title='Управление кружками пользователя')
 
-@app.route('/user/<username>')
-@login_required
-def user(username):
-    user = User.query.filter_by(username=username).first_or_404()
-    return render_template('account.html', user=user)
-
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = SignUpForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data)
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratulations, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('signup.html', title='Зарегистрироваться', form=form)
+    # TO DO
+    return render_template('signup.html', title='Зарегистрироваться')
+
