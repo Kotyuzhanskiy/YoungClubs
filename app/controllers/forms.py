f2b408c79d5685f725a878ecc6053f14b1d14ee2
Kotyuzhanskiy <32913672+Kotyuzhanskiy@users.noreply.github.com>
2018-04-11 16:19:18 +0300

    add comments to forms.py

--- app/controllers/forms.py
+++ app/controllers/forms.py
@@ -1,18 +1,23 @@
+# Данный файл содержит формы, которые отображаются на страницах сайта
+
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, FormField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from app.models.models import User
 
+# Форма входа на сайт для пользователей из детских учреждений
 class LoginForm(FlaskForm):
     username = StringField('Пользователь', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
     remember_me = BooleanField('Запомнить меня')
     submit = SubmitField('Войти')
 
+# Форма поиска детских учреждений на главной странице для анонимных пользователей
 class SearchForm(FlaskForm):
     search = StringField('Я хочу, чтобы мой ребенок занимался', validators=[DataRequired()])
     submit = SubmitField('Найти учреждение')
 
+# Форма регистрации пользователей из детских учреждений
 class SignUpForm(FlaskForm):
     username = StringField('Пользователь', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
@@ -30,6 +35,7 @@ class SignUpForm(FlaskForm):
         if user is not None:
             raise ValidationError('Email не уникален.')
 
+# Форма расширенного поиска детских учреждений на странице найденных учреждений для анонимных пользователей
 class AdvancedSearchForm(FlaskForm):
     sport = BooleanField('ДЮСШ')
     music = BooleanField('Музыка')
@@ -38,6 +44,7 @@ class AdvancedSearchForm(FlaskForm):
     ttoten = BooleanField('Младшие школьники')
     submit = SubmitField('Найти учреждения')
 
+# Форма добавления детского учреждения в аккаунте пользователя
 class AddClubForm(FlaskForm):
     club = StringField('Название кружка', validators=[DataRequired()])
     institution = StringField('Учреждение', validators=[DataRequired()])
@@ -45,11 +52,13 @@ class AddClubForm(FlaskForm):
     category = FormField(AdvancedSearchForm)
     submit = SubmitField('Добавить кружок')
 
+# Форма выбора города при поиске десткого учреждения на всех страницах
 class CityForm(FlaskForm):
     city = SelectField(
         'Выберите Ваш город',
         choices=[('К', 'Краматорск'), ('С', 'Славянск'), ('Д', 'Дружковка')]
     )
+
 #class CityForm(FlaskForm):
 #    city_id = SelectField(u'Group', coerce=int)
 #def edit_city(request, id):
