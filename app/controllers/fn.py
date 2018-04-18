d364ed5841ae99c119afa1f1c36bb2137d89c8b9
Kotyuzhanskiy <32913672+Kotyuzhanskiy@users.noreply.github.com>
2018-04-18 16:45:46 +0300

    внесение категорий в базу

---
+++ app/controllers/fn.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from app import db
+from app.models.models import User, Club, Institution, Age, Category, Tag, Photo
+from sqlalchemy import Table, Column, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+from pprint import pprint
+
+def format_tags(tags_list):
+    tags_list = tags_list.replace(',','')
+    tags_list = tags_list.split()
+    return tags_list
+
+def format_form_list(item_dict):
+    item_list = []
+    for i in item_dict:
+        if str(item_dict[i]) == 'True':
+            item_list.append(str(i))
+    return item_list
+
+#Функция поиска с Главной странице по одной строке
+def SimpleSearch(search_value):
+    x = Club.query.filter(Club.tags.any(name=search_value)).all()
+    return x
+#Функция расширенного поиска по различным полям (категория, возраст, тэги)
+def AdvancedSearch(search_value):
+    print('f')
+
