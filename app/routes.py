27884582de9187265b29b75e842b72f3f9a31fc7
Potapenko <ushakovamaryna@gmail.com>
2018-04-24 10:24:59 +0000

    суббота: Редактировать, Сохранить, Удалить

--- app/routes.py
+++ app/routes.py
@@ -1,34 +1,60 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, flash, redirect, url_for, request, jsonify
+import os
+from flask import Flask, render_template, flash, redirect, url_for, request, jsonify
 from app import app
-from app.controllers.forms import LoginForm, SearchForm, SignUpForm, AdvancedSearchForm, AddClubForm
+from app.controllers.forms import LoginForm, SearchForm, SignUpForm, AdvancedSearchForm, AddClubForm, CityForm
 from flask_login import current_user, login_user, login_required, logout_user
 from werkzeug.urls import url_parse
+from werkzeug.utils import secure_filename
 from app.models.models import User, Club, Age, Category, Tag, Photo
 from app import db
 from app.controllers.fn import format_tags, format_form_list, SimpleSearch, AgeSearch
 from pprint import pprint
 
+#from sqlalchemy import create_engine
+#db = create_engine("sqlite:///clubs.db")
+
+UPLOAD_FOLDER = 'http://ide50-potapenko.cs50.io:8080/logo/'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+#Функция загрузки других функций при отображении любой страницы
+#@app.before_request
+#def before_request():
+#    cityform = CityForm()
+#    print('--------------DEBUG-1--------------')
+#    print(cityform)
+#    return CityForm()
+
+#@app.context_processor
+#def utility_processor(methods=['GET', 'POST']):
+#    cityform = CityForm()
+#    return dict(cityform=cityform)
+
 #Главная страница
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 @app.route('/index', methods=['GET', 'POST'])
 def index():
+    # TO DO
     form = SearchForm()
+    form2 = AdvancedSearchForm()
+    search_value = form.search.data
+    if form.validate_on_submit():
+        clubs_search_results = SimpleSearch(search_value)
+        for club in clubs_search_results:
+            print(club.id)
+            print(club.name)
+            print(club.snippet)
+        #return render_template('search.html', title='Результаты поиска учреждений', form2=form2)
     return render_template('index.html', title='Добро пожаловать', form=form)
 
 #Страница найденных результатов поиска
-@app.route('/search', methods=['POST'])
+@app.route('/search', methods=['GET', 'POST'])
 def search():
-    form = SearchForm()
-    if form.validate_on_submit():
-        search_value = form.search.data
-        clubs_search_results = SimpleSearch(search_value)
-        pprint(clubs_search_results)
-        return render_template(
-            'search.html', title='Результаты поиска учреждений',
-            clubs_search_results=clubs_search_results, search_value=search_value
-            )
-    return render_template('search.html', title='Результаты поиска учреждений', clubs_search_results=None)
+    # TO DO
+    flash('Search for sucsessful')
+    form = AdvancedSearchForm()
+
+    return render_template('search.html', title='Результаты поиска учреждений', form=form)
 
 #Страница входа для зарегистрированных пользователей из детских учреждений
 @app.route('/login', methods=['GET', 'POST'])
@@ -59,7 +85,7 @@ def logout():
 @login_required
 def account():
     #выбор из clubs и institution. похожие названия колонок переименовывать "i.name as name_i"
-    #user_select = db.engine.execute("SELECT c.*, i.name as name_i FROM clubs c INNER JOIN institutions i ON c.institution_id = i.id WHERE user_id = :user_id", user_id=current_user.id)
+#    user_select = db.execute("SELECT c.*, i.name as name_i FROM clubs c INNER JOIN institutions i ON c.institution_id = i.id WHERE user_id = :user_id", user_id=current_user.id)
     user_select = db.engine.execute("SELECT * FROM clubs WHERE user_id = :user_id", user_id=current_user.id)
     return render_template('account.html', title='Управление кружками пользователя', user_select=user_select)
 
@@ -78,14 +104,20 @@ def signup():
         return redirect(url_for('account'))
     return render_template('signup.html', title='Зарегистрироваться', form=form)
 
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 #Страница (функция?) добавления кружка зарегистрированным пользователем
 @app.route('/addclub', methods=['GET', 'POST'])
 @login_required
 def addclub():
+    #TO DO
     form = AddClubForm()
     if form.validate_on_submit():
         new_club = Club(
             user_id=current_user.id,
+            institution=form.institution.data,
             name=form.name.data,
             snippet=form.snippet.data,
             description=form.description.data,
@@ -98,9 +130,18 @@ def addclub():
             street=form.street.data,
             building=form.building.data,
             room=form.room.data,
-            institution=form.institution.data
+#            start=form.start.data,
+#            finish=form.finish.data,
+#            url_logo=form.url_logo.data,
+            #photos=form.photos.data,
             )
+#        new_institution = Institution(
+#            name=form.institution.data
+#            )
         db.session.add(new_club)
+#        db.session.add(new_institution)
+#        new_institution.clubs.append(new_club)
+#        db.session.add(new_club)
         #Добавление тэгов для клуба в БД
         new_tags_list = format_tags(form.tags.data)
         for tag in new_tags_list:
@@ -131,6 +172,7 @@ def addclub():
                 new_club.ages.append(new_age)
             else:
                 new_club.ages.append(row)
+        #Добавление времени работы для кружка в БД
         db.session.commit()#Подтверждение записи в таблицы БД
         return redirect(url_for('account'))
     return render_template('addclub.html', title='Добавление нового кружка', form=form)
@@ -144,7 +186,7 @@ def editclub(club_id):
 @app.route('/deleteclub/<club_id>', methods=['GET'])
 @login_required
 def deleteclub(club_id):
-    club_del = db.engine.execute("DELETE FROM clubs WHERE id = :club_id", club_id = club_id)
+    db.engine.execute("DELETE FROM clubs WHERE id = :club_id", club_id = club_id)
     return redirect (url_for('account'))
 
 @app.route('/updateclub', methods=['POST'])
@@ -164,19 +206,12 @@ def updateclub():
     street = request.form.get('street')
     building = request.form.get('building')
     room = request.form.get('room')
+    url_logo = request.form.get('url_logo')
     club_update = db.engine.execute(
-        """
-            UPDATE clubs SET name=:name, institution=:institution, leader=:leader,
-            price=:price, snippet=:snippet, description=:description, phone=:phone, web=:web, email=:email,
-            social=:social, street=:street, building=:building, room=:room, url_logo=:url_logo
-            WHERE id = :id
-        """
-        , id = id, name=name, institution=institution, leader=leader, price=price)
+        """UPDATE clubs SET name=:name, institution=:institution, leader=:leader, price=:price, snippet=:snippet,
+        description=:description, phone=:phone, web=:web, email=:email, social=:social, street=:street,
+        building=:building, room=:room, url_logo=:url_logo WHERE id = :id"""
+        , id = id, name=name, institution=institution, leader=leader, price=price, snippet=snippet,
+        description=description, phone=phone, web=web, email=email, social=social, street=street,
+        building=building, room=room, url_logo=url_logo)
     return redirect (url_for('account'))
-
-@app.route('/club/<club_id>', methods=['GET'])
-def getclub(club_id):
-    club = db.session.query(Club).filter(Club.id == club_id).all()
-    club_ages = AgeSearch(club_id)
-    #club = db.engine.execute("SELECT * FROM clubs WHERE id = :club_id", club_id = club_id).fetchall()[0]
-    return render_template('getclub.html', title='Информация о кружке', club=club, club_ages=club_ages)
